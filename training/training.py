bef1dc6baaf666df4603c0448ad82e36fb513747
Đào Minh  Tuấn <tuandm@appota.com>
2024-10-17 13:57:54 +0700

    add training model

--- training/training.py
+++ training/training.py
@@ -21,3 +21,43 @@ def evaluate_model(model, test_dataloader, device):
 
     avg_val_loss = total_val_loss / len(test_dataloader)
     print(f"Validation Loss: {avg_val_loss}")
+
+#training
+def train_model(model, train_dataloader, test_dataloader, tokenizer, epochs=5, batch_size=2, lr=5e-5, checkpoint_dir='../checkpoint'):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    
+    optimizer = AdamW(model.parameters(), lr=lr)
+    
+    wandb.login(key=os.getenv("WANDB_API_KEY"))
+    wandb.init(project="summarization", name='seminar2_kaggle_bartpho_newest')
+
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
+            total_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+            wandb.log({"train_loss": loss.item()})
+        
+        avg_loss = total_loss / len(train_dataloader)
+        print(f"Epoch {epoch + 1}/{epochs}, Loss: {avg_loss}")
+        model.save_pretrained(f'{checkpoint_dir}/epoch_{epoch + 1}')
+        tokenizer.save_pretrained(f'{checkpoint_dir}/epoch_{epoch + 1}')
+        
+        artifact = wandb.Artifact(f'model-bartpho-checkpoint-epoch-{epoch + 1}', type='model')
+        artifact.add_dir(f'{checkpoint_dir}/epoch_{epoch + 1}')
+        wandb.log_artifact(artifact)
+        
+        evaluate_model(model, test_dataloader, device)
+    
+    wandb.finish()
